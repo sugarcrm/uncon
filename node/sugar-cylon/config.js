da678b10ce6733e20d25a035c58b510971314937
Neil Harding <nharding@sugarcrm.com>
2015-04-21 13:50:04 -0700

    config - easy serial port settings for macbooks

--- node/sugar-cylon/config.js
+++ node/sugar-cylon/config.js
@@ -12,7 +12,7 @@ config.instance = {
     // This is the URL to the SugarCRM REST API you will be using
     // You will want to use an IP address if accessing remotely from
     // a Raspberry Pi device.
-    serverUrl: "http://192.168.0.109/Mango/ent/sugarcrm/rest/v10",
+    serverUrl: "http://10.9.5.140/Mango/ent/sugarcrm/rest/v10",
     platform: "mobile",
     timeout: 30
 };
@@ -24,6 +24,8 @@ config.users = {
 
 // Change port to match the serial connection to your Arduino,
 // discover this using the command `gort scan serial`
-config.arduinoPort = '/dev/tty.usbmodem1411';
+config.arduinoPort = '/dev/tty.usbmodem1451';
+// /dev/tty.usbmodem1411 - left usb on macbook
+// /dev/tty.usbmodem1451 - right usb on macbook
 
 module.exports = config;
