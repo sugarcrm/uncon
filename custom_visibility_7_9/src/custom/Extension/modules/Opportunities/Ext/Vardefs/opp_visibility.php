f71802623cd96fd581ec346db0c268f19470cc57
Lauren Schaefer <lschaefer@sugarcrm.com>
2017-12-06 09:41:11 -0500

    Update the custom visibility example to work with 7.7, 7.8, & 7.9

---
+++ custom_visibility_7_9/src/custom/Extension/modules/Opportunities/Ext/Vardefs/opp_visibility.php
@@ -0,0 +1,8 @@
+<?php
+// Copyright 2016 SugarCRM Inc.  Licensed by SugarCRM under the Apache 2.0 license.
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$dictionary['Opportunity']['visibility']['FilterOpportunities'] = true;
